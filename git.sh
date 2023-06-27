721aac9ecd464ab0445d7dad7fa6833499867173
abhinavdubey13 <abhinavdubey13@gmail.com>
2023-06-27 21:19:21 +0530

    .

--- git.sh
+++ git.sh
@@ -1,11 +1,13 @@
-# cd problems_2
-# ./git.sh
+cd problems_2
+./git.sh
 
 
-# cd ..
-# cd problems/
-# ./git.sh
+cd ..
+cd problems/
+./git.sh
 
 
 
+
+cd ..
 git add . && git commit -m '.' && git push
